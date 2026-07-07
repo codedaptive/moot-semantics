d063ae21d05bcf58104ce22e9709852f721a39e1
MOOTx01 Release Automation <release@codedaptive.com>
2026-07-07 23:54:10 +0000

    publish: load moot-semantics libraries v1.0.26
    Source-Repo: mootx01-ee
    Source-Commit: 901cb77c45dc4134b85839cb058a9155a41bf6c4
    Venue: moot-semantics
    Version: 1.0.26
    Generated-By: scripts/lib_publish/publish-libraries.py

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "LatticeLib", targets: ["LatticeLib"])
     ],
     dependencies: [
-        .package(url: "https://github.com/codedaptive/moot-core.git", exact: "1.0.25")
+        .package(url: "https://github.com/codedaptive/moot-core.git", exact: "1.0.26")
     ],
     targets: [
         .target(
